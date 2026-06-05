d0397e6523e1d5fb7d01e21a34d49cc7c3fc8097
markmosobo <mmosobo@gmail.com>
2026-06-05 15:01:12 +0300

    changes

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -4,12 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Contact;
-use Illuminate\Support\Facades\Mail;
 
 class ContactController extends Controller
 {
     /**
-     * Store contact message (already OK)
+     * GET /contacts
+     * Show all contacts
+     */
+    public function index()
+    {
+        return response()->json(
+            Contact::latest()->get()
+        );
+    }
+
+    /**
+     * POST /contacts
+     * Store contact message
      */
     public function store(Request $request)
     {
@@ -20,12 +31,45 @@ class ContactController extends Controller
             'message' => 'required|string',
         ]);
 
-        Contact::create($request->only([
-            'name', 'email', 'phone', 'message'
-        ]));
+        Contact::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'message' => $request->message,
+            'is_read' => false, // make sure this column exists
+        ]);
 
         return response()->json([
             'message' => 'Message received. We will get back to you soon.'
         ]);
-    }   
+    }
+
+    /**
+     * PATCH /contacts/{id}/read
+     * Mark as read
+     */
+    public function markAsRead($id)
+    {
+        $contact = Contact::findOrFail($id);
+        $contact->is_read = true;
+        $contact->save();
+
+        return response()->json([
+            'message' => 'Marked as read'
+        ]);
+    }
+
+    /**
+     * DELETE /contacts/{id}
+     * Delete contact
+     */
+    public function destroy($id)
+    {
+        $contact = Contact::findOrFail($id);
+        $contact->delete();
+
+        return response()->json([
+            'message' => 'Contact deleted successfully'
+        ]);
+    }
 }
